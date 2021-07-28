76ac105c04f32732ce71fdfe5a4a62602281c6f9
Alopezm5 <85761855+Alopezm5@users.noreply.github.com>
2021-07-28 15:00:57 -0500

    Create Cadena.py
    Primeras lineas de código del tema 4 referente a cadenas

---
+++ Cadena.py
@@ -0,0 +1,42 @@
+class Cadena:
+    def __init__(self, cadena=""):
+        self.cadena=cadena
+        self.cadena = input("Ingresar cadena:")
+
+    def recorrerCadena(self):
+        print("La cadena es: '{}'y cuenta con:{} caracteres".format(self.cadena, len(self.cadena)))
+        # for i, c in enumerate(self.cadena):
+        #     print('Caracter: %s' % (c))
+
+    def buscarCaracter(self, buscado=0):
+        self.buscado= buscado
+        con=0
+        self.buscado=input("Ingresar el caracter a ser buscado")
+        for i in range(len(self.cadena)):
+            if (self.cadena[i] == self.buscado):
+                con += 1
+                print("En el caracter: " + str(i))
+            # else:
+            #     print("No hay Coincidencia")
+        print("Existe solo: " + str(con) + " veces el caracter:" + self.buscado)
+
+    def listaPosiciones(self, caracter=""):
+        self.caracter= caracter
+        self.caracter=input("ingrese El caracter a buscar en lista: ")
+        for j, l in enumerate(self.cadena):
+            print("Caracter: %s - Puesto: %i " % (l,j))
+            if self.cadena[j]==caracter:
+                print("El caracter: %s  - Se encuentra en el puesto: %i " % (l,j))
+
+    def listaPalabras(self):
+        pass
+    def cadenaLista(self):
+        pass
+    def insertarDato(self, buscado, posicion):
+        pass
+    def eliminarOcurrencias(self,buscado):
+        pass
+    def retornaValor(self,posicion):
+        pass
+    def concatenarCadena(self,dato):
+        pass
