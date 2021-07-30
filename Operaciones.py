da5e6c7df7609cba793fa5863fbedae378e5b1e8
Alopezm5 <85761855+Alopezm5@users.noreply.github.com>
2021-07-30 01:07:31 -0500

    Update Operaciones.py
    Creaciones de métodos para el menú de operaciones con números

--- Operaciones.py
+++ Operaciones.py
@@ -1,1 +1,35 @@
+class Basico:
+    def numerosN1(n):
+        pass
+
+    def multiplo(numero):
+        pass
+
+    def DivisoresNumero(numero):
+        pass
+
+    def primo(numero):
+        pass
+
+    def perfecto(numero):
+        pass
+
+
+class Intermedio(Basico):
+
+    def numerosN2(n):  # aplicar polimorfismo
+        pass
+
+    def factorial(numero):  #
+        pass
+
+    def fibonacci(n):
+        pass
+
+    def primosGemelos(num1, num2):
+        pass
+
+    def amigos(num1, num2):
+        pass
+
 
