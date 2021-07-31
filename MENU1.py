42f181f217638943613c8b468bbb025fb3261a78
Alopezm5 <85761855+Alopezm5@users.noreply.github.com>
2021-07-31 01:30:13 -0500

    Update MENU1.py
    Agregación de líneas de código del menú 3 de tratamiento de listas y al inicio la importaciones de las lineas donde se ejecutan.

--- MENU1.py
+++ MENU1.py
@@ -1,5 +1,6 @@
 from Calculadora import *
 from Operaciones import*
+from Listas import *
 from Cadena import *
 
 class MENU():
@@ -98,43 +99,46 @@ while not salir:
                 opc2 = menu2.menu()
         salir = False
     elif opc=="3":
-        menu3=MENU("Menu Tratamiento de Lista", ["1)Recorrer y presentar los datos", "2)Buscar un valor en una lista", "3)Retornar una lista con los factoriales", "4)Retornar una lista de números primos", "5)Recorrer una lista de diccionario con notas de alumnos","6)Insertar un dato en una Lista dada lo Posición","7)Eliminar todas las ocurrencias en una Lista","8)Retornar cualquier valor de una lista eliminándolo","9)Copiar cada elemento de una tupla en una lista","10)Dar el vuelto a varios clientes","11)Salir"])
-        opc3=menu3.menu()
-        if opc3=="1":
-            cal=Cadena()
-            cal.recorrerCadena()
-        elif opc3=="2":
-            cal = Calculadora(0,0)
-            cal.resta()
-        elif opc3=="3":
-            cal=Calculadora(0,0)
-            cal.suma()
-        elif opc3=="4":
-            cal = Calculadora(0,0)
-            cal.resta()
-        elif opc3=="5":
-            cal=Calculadora(0,0)
-            cal.suma()
-        elif opc3=="6":
-            cal = Calculadora(0,0)
-            cal.resta()
-        elif opc3=="7":
-            cal=Calculadora(0,0)
-            cal.suma()
-        elif opc3=="8":
-            cal = Calculadora(0,0)
-            cal.resta()
-        elif opc3=="9":
-            cal=Calculadora(0,0)
-            cal.suma()
-        elif opc3=="10":
-            cal = Calculadora(0,0)
-            cal.resta()
-        elif opc3=="11":
-            cal = Calculadora(0,0)
-            cal.resta()
-        else:
-            print("Mal ingreso")
+        while not salir:
+            menu3=MENU("Menu Tratamiento de Lista", ["1)Recorrer y presentar los datos", "2)Buscar un valor en una lista", "3)Retornar una lista con los factoriales", "4)Retornar una lista de números primos", "5)Recorrer una lista de diccionario con notas de alumnos","6)Insertar un dato en una Lista dada lo Posición","7)Eliminar todas las ocurrencias en una Lista","8)Retornar cualquier valor de una lista eliminándolo","9)Copiar cada elemento de una tupla en una lista","10)Dar el vuelto a varios clientes","11)Salir"])
+            opc3=menu3.menu()
+            if opc3=="1":
+                lis=Lista()
+                lis.presentarLista()
+            elif opc3=="2":
+                lis=Lista()
+                lis.buscarLista()
+            elif opc3 == "3":
+                lis = Lista()
+                lis.listaFactorial()
+            elif opc3 == "4":
+                lis = Lista()
+                lis.listaPrimo()
+            elif opc3 == "5":
+                lis = Lista()
+                lis.listaNotas()
+            elif opc3 == "6":
+                lis = Lista()
+                lis.insertarLista()
+            elif opc3 == "7":
+                lis = Lista()
+                lis.eliminarLista()
+            elif opc3 == "8":
+                lis = Lista()
+                lis.retornaValorLista()
+            elif opc3 == "9":
+                lis = Lista()
+                lis.copiarTuplaLista()
+            elif opc3 == "10":
+                lis = Lista()
+                lis.vueltoLista()
+            elif opc3=="11":
+                print("Regresando al menú principal........")
+                salir = True
+            else:
+                print("Ingreso mal la opcion")
+                opc3 = menu3.menu()
+        salir = False
     elif opc=="4":
         while not salir:
             menu4 = MENU("Menu Operaciones de Cadena", ["1)Recorrer y presentar los datos de una cadena", "2)Buscar un cáracter en una cadena","3)Retornar una lista con las posiciones dado un cáracter de la cadena","4)Retornar una lista de todas las palabras de una cadena","5)Retornar una cadena a partir de una lista","6)Insertar un dato en una cadena dada la Posición","7)Eliminar todas las ocurrencias en una cadena","8)Retornar cualquier valor de una lista eliminándolo","9)Concatenar cualquier valor de una cadena eliminandola","10)Salir"])
