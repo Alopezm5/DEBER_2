7c6a051620a3db51bcb5d6e5def0bce10651a01f
JonathanSL28 <85839428+JonathanSL28@users.noreply.github.com>
2021-07-31 11:44:13 -0500

    Update Listas.py
    Líneas de código del método buscar un dato en la lista.

--- Listas.py
+++ Listas.py
@@ -14,3 +14,17 @@ class Lista(Intermedio):
         Cade = " ".join(self.lista)
         print("Los datos ingresados son: \n" + Cade)
         print("La lista es: '{}'y cuenta con:{} caracteres".format(self.lista, len(self.lista)))
+    
+    def buscarLista(self,valor=""):
+        self.lista = self.lista.split()
+        nueva = str(input("Ingresar datos que desea tener en la lista, en caso de ingrear mas ingrese un puntito(.) \n"))
+        while nueva != ".":
+            self.lista.append(nueva)
+            nueva = str(input("Si desea ingresar mas datos hagalo, caso contrario para finaizar ingrese un puntito\n"))
+        print("La lista es: '{}'y cuenta con:{} caracteres".format(self.lista, len(self.lista)))
+        con = 0
+        valor = str(input("Ingresar el dato a ser buscado: "))
+        for i in range(len(self.lista)):
+            if self.lista[i] in valor:
+                con += 1
+        print("Existe solo: " + str(con) + " veces el caracter:" + valor)
