468cf17de35563948663d15192f07ab7f6d79772
JonathanSL28 <85839428+JonathanSL28@users.noreply.github.com>
2021-07-31 11:16:20 -0500

    Update Listas.py
    Primeras líneas de código para presentar lista.

--- Listas.py
+++ Listas.py
@@ -1,1 +1,16 @@
+from Operaciones import *
 
+class Lista(Intermedio):
+    def __init__(self, lista=""):
+        self.lista = lista
+
+
+    def presentarLista(self):
+        self.lista = self.lista.split()
+        nueva = str(input("Ingresar datos que desea tener en la lista, en caso de ingrear mas ingrese un puntito(.) \n"))
+        while nueva != ".":
+            self.lista.append(nueva)
+            nueva = str(input("Si desea ingresar mas datos hagalo, caso contrario para finaizar ingrese un puntito\n"))
+        Cade = " ".join(self.lista)
+        print("Los datos ingresados son: \n" + Cade)
+        print("La lista es: '{}'y cuenta con:{} caracteres".format(self.lista, len(self.lista)))
