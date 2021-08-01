4b74963ef9d212d6e03280752e48f00c58f8cb6b
JonathanSL28 <85839428+JonathanSL28@users.noreply.github.com>
2021-08-01 18:16:11 -0500

    Update Listas.py
    Arreglo 5.

--- Listas.py
+++ Listas.py
@@ -70,16 +70,16 @@ class Lista(Intermedio):
     # def listaNotas(listaNotasDicccionario):
    #     pass
    
-    def insertarLista(valor):
+    def insertarLista(valor="", posicion=0):
         Lista=[]
-        #Valor=str(input("Ingrese una palabra, al finaizar use el punto\n"))
-        while Valor!=".":
-            Lista.append(Valor)
-            Valor=str(input("Ingrese una palabra, al finaizar use el punto\n"))
+        valor=str(input("Ingrese una palabra, al finaizar use el punto\n"))
+        while valor!=".":
+            valor=str(input("Ingrese una palabra, al finaizar use el punto\n"))
+            Lista.append(valor)
         print("La Lista seria\n"+ str(Lista))
         buscado1=str(input("Ingrese lo que desea agregar a la lista\n"))
-        Posicion=int(input("Ingrese en que posicion desea agregar el anadido\n"))
-        Lista.insert(Posicion, str(buscado1))
+        posicion=int(input("Ingrese en que posicion desea agregar el anadido\n"))
+        Lista.insert(posicion, str(buscado1))
         print("La Lista ahora es\n"+ str(Lista))
    
    
